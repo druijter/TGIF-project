7faf2f6710d1586f2c84ccfa641ac7a59dc069b6
Daan Ruijter <daanruijter@gmail.com>
2019-12-16 16:39:20 +0100

    working on the dynamic table

--- index.js
+++ index.js
@@ -1,16 +1,62 @@
 window.onload = function (){
     console.log(JSON.stringify(data,null,2))
-    // document.getElementById("senate-data").innerHTML 
+    
      
     var membercollection = data.results[0].members;
     
+    
+   
+    
+    
     for(i=0; i<membercollection.length; i++){
+        var a = "a"
+        var tr = document.createElement("TR")
+        tr.id = a + i
+        console.log(tr.id)
         
-        console.log(membercollection[i].first_name + " " + (membercollection[i].middle_name || "")+ "" + membercollection[i].last_name)
-        console.log(membercollection[i].party)
-        console.log(membercollection[i].state)
-        console.log(membercollection[i].seniority)
-        console.log(membercollection[i].votes_with_party_pct)
+         document.getElementById("senate-data").appendChild(tr)
+         tr.innerHTML += "ajax" 
+         
+        
+        for(j=0; j<6; j++){
+            var b = "b"
+            var td = document.createElement("td")
+            tr.appendChild(td)
+            td.id = b + j
+            document.getElementById(td.id).innerHTML="test"
+        }
+    
+
+        var row = document.getElementsByTagName(tr)
+        // document.getElementById("senate-data").appendChild(row)
+        //     console.log("hi")
+
+        // var fullname = membercollection[i].first_name + " " + (membercollection[i].middle_name || "")+ "" + membercollection[i].last_name;
+        // var party = membercollection[i].party
+        // var state = membercollection[i].state
+        // var seniority = membercollection[i].seniority
+        // var votes = membercollection[i].votes_with_party_pct
+        
+
+       
+        
+        // document.createElement("TD");
+        // document.getElementsByTagName('td').innerHTML = fullname
+        // tr.appendChild(td)
+        // document.createElement("TD");
+      
+        // td.innerHTML = party
+        
+        
+        
+        
+     
+        
+
+        //  document.getElementById("senate-data").innerHTML += membercollection[i].first_name + " " + (membercollection[i].middle_name || "")+ "" + membercollection[i].last_name +
+        //  " " + membercollection[i].party + " " + membercollection[i].state + " " + membercollection[i].seniority + " " + membercollection[i].votes_against_party_pct + " " 
+
+        // document.getElementById("senate-data").innerHTML = td.innerHTML
 
     }
 }
