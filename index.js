3713eaf8388b2ddf933e86f98b2db3431c4a32b0
Daan Ruijter <daanruijter@gmail.com>
2019-12-17 11:04:25 +0100

    managed to get the dynamic table with the data. Now trying to add the right columns to the right rows

--- index.js
+++ index.js
@@ -1,118 +1,79 @@
-function x (){
-    console.log(JSON.stringify(data,null,2))
-    
-     
+window.onload = function () {
+
+  
+
     var membercollection = data.results[0].members;
-    
-    
-   
-    
-    
-    for(i=0; i<membercollection.length; i++){
-        var a = "a"
+
+
+
+
+
+    for (i = 0; i < membercollection.length; i++) {
+
         var tr = document.createElement("TR")
-        tr.className = a + i
-        console.log(tr.className)
-        
-         document.getElementById("senate-data").appendChild(tr)
 
-        
-         tr.innerHTML += " testt"
-        
-         
 
-       
-        for(j=0; j<5; j++){
-           
-            var b = "b"
-                var td = document.createElement("td")
-                tr.appendChild(td)
-                td.className = b + j
-                console.log("hi")
-            function tdCreator (){
-                // document.getElementById('b0').innerHTML = "test"
-                // b0.innerHTML = "hi" 
 
-            // if (td.id === "b0") {
-            //     console.log (td.id)
-            //     b0.innerHTML += "hi"
-                
-            // // }
-            // if (td.className === "b1") {
-            //     console.log (td.id)
-            //     b1.innerHTML += "hi2"
-                
-            // }
-            // if (td.className === "b2") {
-            //     console.log (td.id)
-            //     b2.innerHTML += "hi3"
-                
-            // }
-            // if (td.className === "b3") {
-            //     console.log (td.id)
-            //     b3.innerHTML += "hi4"
-                
-            // }
-            // if (td.className === "b4") {
-            //     console.log (td.id)
-            //     b4.innerHTML += "hi5"
-                
-            // }
+        document.getElementById("senate-data").appendChild(tr)
+
+
+
+        for (j = 0; j < 5; j++) {
+
+            var thstijl1 = document.getElementById("thstijl1") 
+            var thstijl2 = document.getElementById('thstijl2')
+            var thstijl3 = document.getElementById('thstijl3')
+            var thstijl4 = document.getElementById('thstijl4')
+            var thstijl5 = document.getElementById('thstijl5')
+
+
+
+            var td = document.createElement("td")
+            tr.appendChild(td)
             
-            
+            if (j == 0) {
+                td.innerHTML = membercollection[i].first_name + " " + (membercollection[i].middle_name || "") + "" + membercollection[i].last_name
+                    
             }
-            tdCreator ()
-            
-        
+            if (j == 1) {
+                td.innerHTML = membercollection[i].party
+            }
+            if (j == 2) {
+                td.innerHTML = membercollection[i].state
+            }
+            if (j == 3) {
+                td.innerHTML = membercollection[i].seniority
+            }
+            if (j == 4) {
+                td.innerHTML = membercollection[i].votes_with_party_pct
+            } else {
+                null
+            }
+
+
+
+
         }
 
-
-
-
-        // var row = document.getElementsByTagName(tr)
-        // document.getElementById("senate-data").appendChild(row)
-        //     console.log("hi")
-
-        // var fullname = membercollection[i].first_name + " " + (membercollection[i].middle_name || "")+ "" + membercollection[i].last_name;
-        // var party = membercollection[i].party
-        // var state = membercollection[i].state
-        // var seniority = membercollection[i].seniority
-        // var votes = membercollection[i].votes_with_party_pct
-        
-
-       
-        
-        // document.createElement("TD");
-        // document.getElementsByTagName('td').innerHTML = fullname
-        // tr.appendChild(td)
-        // document.createElement("TD");
-      
-        // td.innerHTML = party
-        
-        
-        
-        
-     
-        
-
-        //  document.getElementById("senate-data").innerHTML += membercollection[i].first_name + " " + (membercollection[i].middle_name || "")+ "" + membercollection[i].last_name +
-        //  " " + membercollection[i].party + " " + membercollection[i].state + " " + membercollection[i].seniority + " " + membercollection[i].votes_against_party_pct + " " 
-
-        // document.getElementById("senate-data").innerHTML = td.innerHTML
-
-
-    
-// for (k=0 k<r.length)
     }
 
-    
-    var r = document.getElementsByClassName('a0')
-    console.log(r)
 
 }
 
-x();
 
 
 
 
+
+
+
+// var fullname = membercollection[i].first_name + " " + (membercollection[i].middle_name || "")+ "" + membercollection[i].last_name;
+// var party = membercollection[i].party
+// var state = membercollection[i].state
+// var seniority = membercollection[i].seniority
+// var votes = membercollection[i].votes_with_party_pct
+
+//  document.getElementById("senate-data").innerHTML += membercollection[i].first_name + " " + (membercollection[i].middle_name || "")+ "" + membercollection[i].last_name +
+//  " " + membercollection[i].party + " " + membercollection[i].state + " " + membercollection[i].seniority + " " + membercollection[i].votes_against_party_pct + " " 
+
+// document.getElementById("senate-data").innerHTML = td.innerHTML
