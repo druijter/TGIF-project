6dbccef0154b513b36570dc2b9597b13ada8e10f
Daan Ruijter <daanruijter@gmail.com>
2019-12-18 14:32:07 +0100

    changed project document structure and working on the statistics object for the senate attendance page

---
+++ statistics.js
@@ -0,0 +1,44 @@
+let statistics = {
+  "overall": {
+    democrats: [{
+        "democratsnum": "",
+        "demvotepartypct": ""
+      }
+
+    ],
+    republicans: [{
+      "republicansnum": "",
+      "repvotepartypct": ""
+    }
+
+  ],
+  independents: [{
+    "independentsnum": "",
+    "indepvotepartypct": ""
+  }
+
+],
+
+
+  },
+
+
+  "leastengaged": [{
+    "name": "",
+    "missedvotesnum": "",
+    "missedpct": ""
+  }],
+
+  "mostengaged": [{
+    "name": "",
+    "missedvotesnum": "",
+    "missedpct": ""
+  }]
+
+}
+
+
+
+
+
+
