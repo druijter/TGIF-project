7322c6f940dd9a42f8c7036361acf1a33b7efaf2
Daan Ruijter <daanruijter@gmail.com>
2019-12-18 15:31:04 +0100

    created the empty statistics object and filled it already with num republicans, num democrats, num independents

--- statistics.js
+++ statistics.js
@@ -1,20 +1,41 @@
+let republicans = []
+let democrats = []
+let independents = []
+
+var senatemembers = data.results[0].members
+for (i=0; i<senatemembers.length; i++){
+
+  
+  if(senatemembers[i].party==="R")
+  republicans.push(senatemembers[i])
+  // console.log( senatemembers[i])
+  if(senatemembers[i].party==="D")
+  democrats.push(senatemembers[i])
+  // console.log( senatemembers[i])
+  if(senatemembers[i].party==="I")
+  independents.push(senatemembers[i])
+  // console.log( senatemembers[i])
+}
+
+
+
 let statistics = {
   "overall": {
     democrats: [{
-        "democratsnum": "",
-        "demvotepartypct": ""
+        "democratsnum": democrats.length,
+        "demvotepartypct": 0
       }
 
     ],
     republicans: [{
-      "republicansnum": "",
-      "repvotepartypct": ""
+      "republicansnum": republicans.length,
+      "repvotepartypct": 0
     }
 
   ],
   independents: [{
-    "independentsnum": "",
-    "indepvotepartypct": ""
+    "independentsnum": independents.length,
+    "indepvotepartypct": 0
   }
 
 ],
@@ -24,18 +45,41 @@ let statistics = {
 
 
   "leastengaged": [{
-    "name": "",
-    "missedvotesnum": "",
-    "missedpct": ""
+    "name": 0,
+    "missedvotesnum": 0,
+    "missedpct": 0
   }],
 
   "mostengaged": [{
-    "name": "",
-    "missedvotesnum": "",
-    "missedpct": ""
+    "name": 0,
+    "missedvotesnum": 0,
+    "missedpct": 0
   }]
 
 }
+
+
+console.log(statistics)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
