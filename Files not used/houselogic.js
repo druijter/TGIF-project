e020c8a0d6ee521e3654a86ae38adec2988a6ac7
Daan Ruijter <daanruijter@gmail.com>
2019-12-18 12:21:59 +0100

    have one js-file for both tables now. It works

---
+++ Files not used/houselogic.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+
+    let memberCollection = data.results[0].members;
+
+
+    this.createTable(memberCollection)
+
+
+}
+
+function createTable (memberCollection) {
+
+    const tbody = document.getElementById("table-data");
+
+    for (i = 0; i < memberCollection.length; i++) {
+
+
+        let tr = document.createElement("TR")
+
+        tbody.appendChild(tr)
+
+        let a = document.createElement("a")
+        a.href = memberCollection[i].url
+        a.innerHTML = `${memberCollection[i].first_name} ${(memberCollection[i].middle_name || " ")}  ${memberCollection[i].last_name}`
+       
+
+        tr.insertCell().appendChild(a) 
+        tr.insertCell().innerHTML = memberCollection[i].party
+        tr.insertCell().innerHTML = memberCollection[i].state
+        tr.insertCell().innerHTML = memberCollection[i].seniority
+        tr.insertCell().innerHTML = memberCollection[i].votes_with_party_pct
+
+
+        
+
+    }
+
+    
+}
