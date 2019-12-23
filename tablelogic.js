12fec5e5037fe04d42c6dfc9d2c12a08852d35a5
Daan Ruijter <daanruijter@gmail.com>
2019-12-23 18:16:47 +0100

    calling the function extractPartymembers 3 times ruin the tables, because I see the members 3 times pushed. Now solving that

--- tablelogic.js
+++ tablelogic.js
@@ -54,136 +54,127 @@ window.onload = function () {
             for (i = 0; i < senateMembers.length; i++) {
 
 
-                if (senateMembers[i].party === "R")
+                if (senateMembers[i].party === "R") {
                     republicans.push(senateMembers[i])
-
-                if (senateMembers[i].party === "D")
-                    democrats.push(senateMembers[i])
-
-                if (senateMembers[i].party === "I")
-                    independents.push(senateMembers[i])
-
-            }
-            return {
-                republicans,
-                democrats,
-                independents
-
-            }
-        }
-
-        //set variables to their objects//
-        republicans = extractPartyMembers().republicans
-        democrats = extractPartyMembers().democrats
-        independents = extractPartyMembers().independents
-
-
-        //default table: no checkbox is checked//
-        function createTablePerParty(memberCollection) {
-
-            const tbody = document.getElementById("table-data");
-
-            for (i = 0; i < memberCollection.length; i++) {
-
-
-                let tr = document.createElement("TR")
-
-                tbody.appendChild(tr)
-
-                let a = document.createElement("a")
-                a.href = memberCollection[i].url
-                a.innerHTML = `${memberCollection[i].first_name} ${(memberCollection[i].middle_name || " ")}  ${memberCollection[i].last_name}`
-
-
-                tr.insertCell().appendChild(a)
-                tr.insertCell().innerHTML = memberCollection[i].party
-                tr.insertCell().innerHTML = memberCollection[i].state
-                tr.insertCell().innerHTML = memberCollection[i].seniority
-                tr.insertCell().innerHTML = memberCollection[i].votes_with_party_pct
-
-
-
-
+                }
             }
 
-
-        }
-
-
-        //if the democrats checkbox is checked, create the democrat table and replace the default overall table//
-        // if (document.getElementById("democratFilter")) {
+            for (j = 0; j < senateMembers.length; j++) {
 
 
-        //     document.getElementById("democratFilter").onclick = function () {
-        //         for (i = 0; i < memberCollection.length; i++) {
-        //             document.getElementById("table-data").deleteRow();
-        //             console.log("hi")
-
-        //         }
-        //     }
-        //     createTablePerParty(democrats)
-        // }
-        // }
-
-
-
-
-
-        //if the democrats checkbox is checked, create the democrat table and replace the default overall table//
-        // if (document.getElementById("independentFilter")) {
-        //     console.log("hi")
-
-        //     document.getElementById("independentFilter").onclick = function () {
-        //         for (i = 0; i < memberCollection.length; i++) {
-        //             document.getElementById("table-data").deleteRow(0);
-        //             console.log("hi")
-
-        //         }
-        //     }
-
-
-
-        // }
-
-        //if nothing is checked, show the default table//
-
-        // if (document.getElementById("democratFilter")){
-
-
-
-
-        // }
-
-        // let clickedArray = []
-
-
-        let democratsClicked = document.getElementById("democratFilter")
-
-        democratsClicked.onclick = function (e) {
-
-           
-            if (e.target.checked === true) {
-                console.log("isfalse")
-                for (i = 0; i < memberCollection.length; i++) {
-                    document.getElementById("table-data").deleteRow(0);
-                    
+                if (senateMembers[j].party === "D") {
+                    democrats.push(senateMembers[j])
+                }
+            }
+            for (k = 0; k < senateMembers.length; k++) {
+                if (senateMembers[k].party === "I") {
+                    independents.push(senateMembers[k])
+                }
+            }
+                return {
+                    republicans,
+                    democrats,
+                    independents
 
                 }
-                createTablePerParty(memberCollection)
             }
+
+            //set variables to their objects//
+            republicans = extractPartyMembers().republicans
+            democrats = extractPartyMembers().democrats
+            independents = extractPartyMembers().independents
+            console.log(independents[0].first_name)
+
+            //default table: no checkbox is checked//
+            function createTablePerParty(memberCollection) {
+
+                const tbody = document.getElementById("table-data");
+
+                for (i = 0; i < memberCollection.length; i++) {
+
+
+                    let tr = document.createElement("TR")
+
+                    tbody.appendChild(tr)
+
+                    let a = document.createElement("a")
+                    a.href = memberCollection[i].url
+                    a.innerHTML = `${memberCollection[i].first_name} ${(memberCollection[i].middle_name || " ")}  ${memberCollection[i].last_name}`
+
+
+                    tr.insertCell().appendChild(a)
+                    tr.insertCell().innerHTML = memberCollection[i].party
+                    tr.insertCell().innerHTML = memberCollection[i].state
+                    tr.insertCell().innerHTML = memberCollection[i].seniority
+                    tr.insertCell().innerHTML = memberCollection[i].votes_with_party_pct
+
+
+
+
+                }
+
+
+            }
+
+
+            //when checkboxes are checked//
+
+            let democratsClicked = document.getElementById("democratFilter")
+
+            democratsClicked.onclick = function (e) {
+
+
+                if (e.target.checked === true) {
+                    console.log("isfalse")
+                    for (i = 0; i < memberCollection.length; i++) {
+                        document.getElementById("table-data").deleteRow(0);
+
+
+                    }
+                    createTablePerParty(memberCollection)
+                }
                 if (e.target.checked !== true) {
                     console.log("istrue")
                     for (i = 0; i < memberCollection.length; i++) {
-                        document.getElementById("table-data").deleteRow(0);}
-                    
-    
+                        document.getElementById("table-data").deleteRow(0);
+                    }
+
+
+                }
+
+                createTable(memberCollection)
+            }
+
+            let independentsClicked = document.getElementById("independentFilter")
+
+            independentsClicked.onclick = function (e) {
+
+
+                if (e.target.checked === true) {
+                    console.log("hij is gechecked")
+                    for (i = 0; i < memberCollection.length; i++) {
+                        document.getElementById("table-data").deleteRow(0);
+
+
+                    }
+                    console.log("create the table")
+
+                    createTablePerParty(independents)
+
+
+                }
+                if (e.target.checked !== true) {
+                    console.log("hij is niet gechecked")
+                    for (i = 0; i < independents.length; i++) {
+                        document.getElementById("table-data").deleteRow(0);
                     }
 
                     createTable(memberCollection)
+
+
+                }
+
+
             }
+
         }
-
-
-
-
-        
