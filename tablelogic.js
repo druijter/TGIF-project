42262de272ddbb23741967a774197810dccff490
Daan Ruijter <daanruijter@gmail.com>
2020-01-09 17:38:26 +0100

    logic works again. Still one function to define below

--- tablelogic.js
+++ tablelogic.js
@@ -77,301 +77,102 @@ function executeAttendanceAndLoyaltyTables(memberCollection) {
   let independents = []
   let overallVoteStatistics = []
 
-  //extract data in array in variables//
-  function extractPartyMembers() {
-
-    let senateMembers = memberCollection
-    for (i = 0; i < senateMembers.length; i++) {
+  var partyMembersObject = extractPartyMembers(republicans, democrats, independents)
 
 
-      if (senateMembers[i].party === "R")
-        republicans.push(senateMembers[i])
-
-      if (senateMembers[i].party === "D")
-        democrats.push(senateMembers[i])
-
-      if (senateMembers[i].party === "I")
-        independents.push(senateMembers[i])
-
-    }
-    return {
-      republicans,
-      democrats,
-      independents
-
-    }
-  }
+  const overallVoteStatisticsObject = getOverallVoteStatistics(overallVoteStatistics, memberCollection)
 
 
 
-
-  // making a general function to calculate the average per party
-  function calculateAveragePerParty(partyTypeArray, datatype) {
-    let total = 0
-    for (j = 0; j < partyTypeArray.length; j++) {
-
-
-      total += partyTypeArray[j][datatype]
-
-
-
-    }
-
-    let average = total / (partyTypeArray.length)
-    total = null
-    return average
-
-  }
-
-
-
-  // // making a general function to calculate the overall average 
-  // function calculateOverallAverage(datatype) {
-
-  //   let overallTotal = 0
-  //   for (j = 0; j < datatype.length; j++) {
-
-
-  //     overallTotal += data.results[0].members[j][datatype]
-
-
-
-  //   }
-
-  //   let overallAverage = overallTotal / (datatype.length)
-  //   overallTotal = null
-  //   return overallAverage
-
-  // }
-
-
-
-  //get overall vote statistics//
-
-  function getOverallVoteStatistics() {
-
-
-    for (i = 0; i < senateMembersStatistics.length; i++) {
-      overallVoteStatistics.push({
-        "name": `${senateMembersStatistics[i].first_name} ${(senateMembersStatistics[i].middle_name || " ")}  ${senateMembersStatistics[i].last_name}`,
-        "missedVotesNum": senateMembersStatistics[i].missed_votes,
-        "missedPct": senateMembersStatistics[i].missed_votes_pct.toFixed(1)
-      })
-    }
+  let sortedOnMissedVotesDescending = [...overallVoteStatisticsObject].sort(function (a, b) {
+    return parseFloat(b.missedVotesNum) - parseFloat(a.missedVotesNum);
+  })
 
 
 
 
 
-    const overallVoteStatisticsObject = overallVoteStatistics
-    console.log(overallVoteStatisticsObject)
+  //create most engaged object//
+
+  let sortedOnMissedVotesAscending = [...overallVoteStatisticsObject].sort(function (a, b) {
+    return parseFloat(a.missedVotesNum) - parseFloat(b.missedVotesNum);
+  })
 
 
 
+  const percentage = 0.10 * senateMembersStatistics.length
 
-    let sortedOnMissedVotesDescending = [...overallVoteStatisticsObject].sort(function (a, b) {
-      return parseFloat(b.missedVotesNum) - parseFloat(a.missedVotesNum);
-    })
+  let lastPersonMissedVotes = sortedOnMissedVotesAscending[Math.round(percentage)].missedVotesNum
 
+  let firstPersonOutMissedVotes = sortedOnMissedVotesAscending[Math.round(percentage + 1)].missedVotesNum
 
-
-
-
-
-    //create most engaged object//
-
-    let sortedOnMissedVotesAscending = [...overallVoteStatisticsObject].sort(function (a, b) {
-      return parseFloat(a.missedVotesNum) - parseFloat(b.missedVotesNum);
-    })
+  var test = returnEngagement(senateMembersStatistics, sortedOnMissedVotesDescending, sortedOnMissedVotesAscending, percentage, lastPersonMissedVotes)
+ 
 
 
-
-
-
-    const percentage = 0.10 * senateMembersStatistics.length
-
-    let lastPersonMissedVotes = sortedOnMissedVotesAscending[Math.round(percentage)].missedVotesNum
-
-    let firstPersonOutMissedVotes = sortedOnMissedVotesAscending[Math.round(percentage + 1)].missedVotesNum
-    console.log(lastPersonMissedVotes)
-
-    return {
-      sortedOnMissedVotesAscending,
-      sortedOnMissedVotesDescending,
-      percentage,
-      lastPersonMissedVotes
-    }
-  }
-
-  // console.log(getOverallVoteStatistics())
   //get overall loyalty statistics
 
-  function getOverallLoyaltyStatistics() {
-    let overallLoyaltyStatistics = []
-    for (i = 0; i < senateMembersStatistics.length; i++) {
-      overallLoyaltyStatistics.push({
-        "name": `${senateMembersStatistics[i].first_name} ${(senateMembersStatistics[i].middle_name || " ")}  ${senateMembersStatistics[i].last_name}`,
-        "partyVotesNum": `${senateMembersStatistics[i].total_votes}`,
-        "partyVotesPct": senateMembersStatistics[i].votes_with_party_pct.toFixed(1)
-      })
-    }
-    return overallLoyaltyStatistics
+  getOverallLoyaltyStatistics(senateMembersStatistics)
+
+
+console.log(returnEngagement(senateMembersStatistics, sortedOnMissedVotesDescending, sortedOnMissedVotesAscending, percentage, lastPersonMissedVotes))
+
+  let statistics = {}
+
+
+console.log(statistics)
+   
+   
+   statistics.overall = {
+
+    numberOfRepresentatives: [
+      partyMembersObject.democrats.length,
+      partyMembersObject.republicans.length,
+      partyMembersObject.independents.length
+    ],
   }
 
+  statistics.votedWithParty =
+  [
+    calculateAveragePerParty(democrats, "votes_with_party_pct").toFixed(1),
+    calculateAveragePerParty(republicans, "votes_with_party_pct").toFixed(1),
+    calculateAveragePerParty(independents, "votes_with_party_pct").toFixed(1)
+  ],
+
+  statistics.missedVotePerParty =
+  [
+    calculateAveragePerParty(democrats, "missed_votes_pct"),
+    calculateAveragePerParty(republicans, "missed_votes_pct"),
+    calculateAveragePerParty(independents, "missed_votes_pct")
+  ]
+
+
+  
+  statistics.leastEngaged  = test.engagement.leastEngaged
+
+  statistics.mostEngaged =test.engagement.mostEngaged
+
+statistics.leastLoyalSenate = test.loyalty.leastLoyal
+
+  statistics.mostLoyalSenate = test.loyalty.mostLoyal
+
+  // console.log(statistics)
+    
 
 
 
 
-  function returnEngagement() {
-    let overallLoyaltyStatisticsObject = getOverallLoyaltyStatistics()
-
-
-    //now sort the array ascending and descending//
-    let sortedOnVotesNumAscending = [...overallLoyaltyStatisticsObject].sort(function (a, b) {
-      return parseFloat(a.partyVotesNum) - parseFloat(b.partyVotesNum);
-    })
 
 
 
-    let sortedOnVotesNumDescending = [...overallLoyaltyStatisticsObject].sort(function (a, b) {
-      return parseFloat(b.partyVotesNum) - parseFloat(a.partyVotesNum);
-    })
-    let sortedArray = getOverallVoteStatistics()
-    let sortedArrayOnVotesAscending = sortedArray.sortedOnMissedVotesAscending
-    let sortedArrayOnVotesDescending = sortedArray.sortedOnMissedVotesDescending
-    // console.log(sortedArrayOnVotes)
-
-    let sortedOnMissedVotesAscending = sortedArrayOnVotesAscending
-    let sortedOnMissedVotesDescending = sortedArrayOnVotesDescending
-    let percentage = sortedArray.percentage
-    let lastPersonMissedVotes = sortedArray.lastPersonMissedVotes
-
-    let leastEngaged = [...sortedOnMissedVotesDescending].slice(0, (Math.round(percentage + 1)))
-    let mostEngaged = [...sortedOnMissedVotesAscending].slice(0, (Math.round(percentage + 1)))
-
-    //variables for party loyalty//
-    let leastLoyal = [...sortedOnVotesNumAscending].slice(0, (Math.round(percentage + 1)))
-    let mostLoyal = [...sortedOnVotesNumDescending].slice(0, (Math.round(percentage + 1)))
 
 
 
-    for (i = (Math.round(percentage) + 1); i < senateMembersStatistics.length - (Math.round(percentage)); i++) {
-      if (lastPersonMissedVotes === sortedOnMissedVotesAscending[i].missedVotesNum) {
-
-        leastEngaged.push({
-          "name": `${sortedOnMissedVotesDescending[i].name}`,
-          "missedVotesNum": sortedOnMissedVotesDescending[i].missedVotesNum,
-          "missedPct": sortedOnMissedVotesDescending[i].missedPct
-        })
-        mostEngaged.push({
-          "name": `${sortedOnMissedVotesAscending[i].name}`,
-          "missedVotesNum": sortedOnMissedVotesAscending[i].missedVotesNum,
-          "missedPct": sortedOnMissedVotesAscending[i].missedPct
-        })
-        mostLoyal.push({
-          "name": `${senateMembersStatistics[i].first_name} ${(senateMembersStatistics[i].middle_name || " ")}  ${senateMembersStatistics[i].last_name}`,
-          "partyVotesNum": `${((senateMembersStatistics[i].votes_with_party_pct)/100)*senateMembersStatistics[i].total_votes}`,
-          "partyVotesPct": senateMembersStatistics[i].votes_with_party_pct.toFixed(1)
-        })
-        leastLoyal.push({
-          "name": `${senateMembersStatistics[i].first_name} ${(senateMembersStatistics[i].middle_name || " ")}  ${senateMembersStatistics[i].last_name}`,
-          "partyVotesNum": `${((senateMembersStatistics[i].votes_with_party_pct)/100)*senateMembersStatistics[i].total_votes}`,
-          "partyVotesPct": senateMembersStatistics[i].votes_with_party_pct
-        })
-
-      } else {
-        break
-      }
-    }
-    return {
-      engagement: {
-        leastEngaged: leastEngaged,
-        mostEngaged: mostEngaged
-      },
-      loyalty: {
-        leastLoyal: leastLoyal,
-        mostLoyal: mostLoyal
-      }
-    }
-  }
-
-function createObjectWithAllAttendanceAndLoyaltyData(){
-
-  var test = returnEngagement()
-  var partyMembersObject = extractPartyMembers()
-
-
-  let statistics = {
-    "overall": {
-
-      numberOfRepresentatives: [
-        partyMembersObject.democrats.length,
-        partyMembersObject.republicans.length,
-        partyMembersObject.independents.length
-      ],
-
-      votedWithParty: [
-        calculateAveragePerParty(democrats, "votes_with_party_pct").toFixed(1),
-        calculateAveragePerParty(republicans, "votes_with_party_pct").toFixed(1),
-        calculateAveragePerParty(independents, "votes_with_party_pct").toFixed(1)
-      ],
-
-      missedVotePerParty: [
-        calculateAveragePerParty(democrats, "missed_votes_pct"),
-        calculateAveragePerParty(republicans, "missed_votes_pct"),
-        calculateAveragePerParty(independents, "missed_votes_pct")
-      ]
-
-    },
-
-
-    "leastEngaged": test.engagement.leastEngaged,
-
-    "mostEngaged": test.engagement.mostEngaged,
-
-    "leastLoyalSenate": test.loyalty.leastLoyal,
-
-    "mostLoyalSenate": test.loyalty.mostLoyal,
-
-  }
-  return statistics
-}
-
-
-
-  function createGlanceTable(tableVariable, dataToShowcolumn2, dataToShowcolumn3) {
-
-    let statistics = createObjectWithAllAttendanceAndLoyaltyData()
-    for (m = 0; m < 3; m++) {
-
-
-
-      let tr = document.createElement('tr')
-
-      if (m == 0) {
-        tr.innerHTML = "Republican"
-      }
-      if (m == 1) {
-
-        tr.innerHTML = "Democrat"
-      }
-      if (m == 2) {
-
-        tr.innerHTML = "Independent"
-      }
-      tableVariable.appendChild(tr)
-      tr.insertCell().innerHTML = statistics.overall[dataToShowcolumn2][m]
-      tr.insertCell().innerHTML = statistics.overall[dataToShowcolumn3][m]
-
-
-
-    }
-  }
 
 
   //function that creates all tables//
-  function createTheTables() {
-
+  function createTheTables(statistics) {
+console.log(statistics)
     //create the Senate at a glance table//
 
     let tableBody = document.getElementById("attendance-table")
@@ -380,7 +181,7 @@ function createObjectWithAllAttendanceAndLoyaltyData(){
 
     if (tableBody !== null) {
 
-      createGlanceTable(tableBody, "numberOfRepresentatives", "votedWithParty")
+      createGlanceTable(tableBody, "numberOfRepresentatives", "votedWithParty",statistics)
 
 
     }
@@ -390,16 +191,16 @@ function createObjectWithAllAttendanceAndLoyaltyData(){
     if (tableBody2 !== null) {
 
 
-      createGlanceTable(tableBody2, "numberOfRepresentatives", "votedWithParty")
+      createGlanceTable(tableBody2, "numberOfRepresentatives", "votedWithParty",statistics)
     }
 
 
     //set the party statistics function to create the tables//
     function createPartyStatisticsTable(tableVariable, engagement, dataToShowcolumn1, dataToShowcolumn2, dataToShowcolumn3) {
-      let statistics = createObjectWithAllAttendanceAndLoyaltyData()
+     
       for (m = 0; m < statistics[engagement].length; m++) {
 
-
+        // console.log(statistics[engagement][m][dataToShowcolumn1])
 
         let tr = document.createElement('tr')
 
@@ -454,7 +255,7 @@ function createObjectWithAllAttendanceAndLoyaltyData(){
       createPartyStatisticsTable(tableBody6, "mostLoyalSenate", "name", "partyVotesNum", "partyVotesPct")
     }
   }
-  createTheTables()
+  createTheTables(statistics)
 }
 
 
@@ -588,8 +389,177 @@ function createDropdownMenu() {
 
 //FUNCTION DEFINITIONS FOR THE ATTENDANCE AND LOYALTY PART (senate attendance and loyalty page and house attendance and loyalty page)//
 
+function extractPartyMembers(republicans, democrats, independents) {
+
+  let senateMembers = memberCollection
+  for (i = 0; i < senateMembers.length; i++) {
 
 
+    if (senateMembers[i].party === "R")
+      republicans.push(senateMembers[i])
+
+    if (senateMembers[i].party === "D")
+      democrats.push(senateMembers[i])
+
+    if (senateMembers[i].party === "I")
+      independents.push(senateMembers[i])
+
+  }
+  return {
+    republicans,
+    democrats,
+    independents
+
+  }
+}
+
+
+// making a general function to calculate the average per party
+function calculateAveragePerParty(partyTypeArray, datatype) {
+  let total = 0
+  for (j = 0; j < partyTypeArray.length; j++) {
+
+
+    total += partyTypeArray[j][datatype]
+
+
+
+  }
+
+  let average = total / (partyTypeArray.length)
+  total = null
+  return average
+
+}
+
+
+//get overall vote statistics//
+
+function getOverallVoteStatistics(overallVoteStatistics, memberCollection) {
+  let senateMembersStatistics = memberCollection
+  console.log(memberCollection)
+
+  for (i = 0; i < senateMembersStatistics.length; i++) {
+    overallVoteStatistics.push({
+      "name": `${senateMembersStatistics[i].first_name} ${(senateMembersStatistics[i].middle_name || " ")}  ${senateMembersStatistics[i].last_name}`,
+      "missedVotesNum": senateMembersStatistics[i].missed_votes,
+      "missedPct": senateMembersStatistics[i].missed_votes_pct.toFixed(1)
+    })
+  }
+  return overallVoteStatistics
+}
+
+
+//get overall loyalty statistics
+
+function getOverallLoyaltyStatistics(memberCollection) {
+  let senateMembersStatistics = memberCollection
+  let overallLoyaltyStatistics = []
+  console.log(senateMembersStatistics)
+  for (i = 0; i < senateMembersStatistics.length; i++) {
+    overallLoyaltyStatistics.push({
+      "name": `${senateMembersStatistics[i].first_name} ${(senateMembersStatistics[i].middle_name || " ")}  ${senateMembersStatistics[i].last_name}`,
+      "partyVotesNum": `${senateMembersStatistics[i].total_votes}`,
+      "partyVotesPct": senateMembersStatistics[i].votes_with_party_pct.toFixed(1)
+    })
+  }
+  return overallLoyaltyStatistics
+}
+
+function returnEngagement(senateMembersStatistics, sortedOnMissedVotesDescending, sortedOnMissedVotesAscending, percentage, lastPersonMissedVotes) {
+
+  let overallLoyaltyStatisticsObject = getOverallLoyaltyStatistics(senateMembersStatistics)
+
+
+  //now sort the array ascending and descending//
+  let sortedOnVotesNumAscending = [...overallLoyaltyStatisticsObject].sort(function (a, b) {
+    return parseFloat(a.partyVotesNum) - parseFloat(b.partyVotesNum);
+  })
+
+
+
+  let sortedOnVotesNumDescending = [...overallLoyaltyStatisticsObject].sort(function (a, b) {
+    return parseFloat(b.partyVotesNum) - parseFloat(a.partyVotesNum);
+  })
+
+
+  let leastEngaged = [...sortedOnMissedVotesDescending].slice(0, (Math.round(percentage + 1)))
+  let mostEngaged = [...sortedOnMissedVotesAscending].slice(0, (Math.round(percentage + 1)))
+
+  //variables for party loyalty//
+  let leastLoyal = [...sortedOnVotesNumAscending].slice(0, (Math.round(percentage + 1)))
+  let mostLoyal = [...sortedOnVotesNumDescending].slice(0, (Math.round(percentage + 1)))
+
+
+
+  for (i = (Math.round(percentage) + 1); i < senateMembersStatistics.length - (Math.round(percentage)); i++) {
+    if (lastPersonMissedVotes === sortedOnMissedVotesAscending[i].missedVotesNum) {
+
+      leastEngaged.push({
+        "name": `${sortedOnMissedVotesDescending[i].name}`,
+        "missedVotesNum": sortedOnMissedVotesDescending[i].missedVotesNum,
+        "missedPct": sortedOnMissedVotesDescending[i].missedPct
+      })
+      mostEngaged.push({
+        "name": `${sortedOnMissedVotesAscending[i].name}`,
+        "missedVotesNum": sortedOnMissedVotesAscending[i].missedVotesNum,
+        "missedPct": sortedOnMissedVotesAscending[i].missedPct
+      })
+      mostLoyal.push({
+        "name": `${senateMembersStatistics[i].first_name} ${(senateMembersStatistics[i].middle_name || " ")}  ${senateMembersStatistics[i].last_name}`,
+        "partyVotesNum": `${((senateMembersStatistics[i].votes_with_party_pct)/100)*senateMembersStatistics[i].total_votes}`,
+        "partyVotesPct": senateMembersStatistics[i].votes_with_party_pct.toFixed(1)
+      })
+      leastLoyal.push({
+        "name": `${senateMembersStatistics[i].first_name} ${(senateMembersStatistics[i].middle_name || " ")}  ${senateMembersStatistics[i].last_name}`,
+        "partyVotesNum": `${((senateMembersStatistics[i].votes_with_party_pct)/100)*senateMembersStatistics[i].total_votes}`,
+        "partyVotesPct": senateMembersStatistics[i].votes_with_party_pct
+      })
+
+    } else {
+      break
+    }
+  }
+  return {
+    engagement: {
+      leastEngaged: leastEngaged,
+      mostEngaged: mostEngaged
+    },
+    loyalty: {
+      leastLoyal: leastLoyal,
+      mostLoyal: mostLoyal
+    }
+  }
+}
+
+function createGlanceTable(tableVariable, dataToShowcolumn2, dataToShowcolumn3, statistics) {
+console.log(dataToShowcolumn3)
+console.log(statistics.votedWithParty)
+  for (m = 0; m < 3; m++) {
+
+
+
+    let tr = document.createElement('tr')
+
+    if (m == 0) {
+      tr.innerHTML = "Republican"
+    }
+    if (m == 1) {
+
+      tr.innerHTML = "Democrat"
+    }
+    if (m == 2) {
+
+      tr.innerHTML = "Independent"
+    }
+    tableVariable.appendChild(tr)
+    tr.insertCell().innerHTML = statistics.overall[dataToShowcolumn2][m]
+    tr.insertCell().innerHTML = statistics.votedWithParty[m]
+
+
+
+  }
+}
 //FECHING AND LOADER PART//
 
 // show loader//
