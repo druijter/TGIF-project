6f0f87e901917e49ad067fa76eddc56d859e9024
Daan Ruijter <daanruijter@gmail.com>
2020-01-10 14:37:16 +0100

    created a file to play around with vue a little bit

---
+++ playWithVue.js
@@ -0,0 +1,19 @@
+// var myObject = new Vue({
+//     el: '#app',
+//     data: {message: 'Hello Vue!'}
+//   })
+
+console.log('jdjdjd');
+
+console.log("hi")
+  
+window.onload = function(){
+console.log("hi")
+// console.log(document.getElementById("lol"))
+}
+
+// document.getElementById("lol").onclick = function myFunction() {
+//     console.log("ih")
+//     myObject.message = "John Doe";
+
+// }
